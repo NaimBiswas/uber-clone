0261d172ed60f71ec8d492f585a6055c63e95827
NaimBiswas <nayeembiswas2@gmail.com>
2021-08-15 15:23:06 +0600

    Uber clone env setup

---
+++ App.js
@@ -0,0 +1,39 @@
+/* eslint-disable prettier/prettier */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+
+import {
+   SafeAreaView,
+   ScrollView,
+   StatusBar,
+   StyleSheet,
+   Text,
+   useColorScheme,
+   View,
+} from 'react-native';
+
+
+
+const App = () => {
+
+   return (
+      <View>
+         <Text>
+            Uber Clone
+        </Text>
+      </View>
+   );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
